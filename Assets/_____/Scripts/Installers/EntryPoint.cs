1bd69df9a847434a0e4da5a400fd0d9b6aaf92ba
romangog <roman@blackgames.space>
2023-05-10 03:22:38 +0400

    MAJOR CLEANUP (and movement restrictions)

---
+++ Assets/_____/Scripts/Installers/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class EntryPoint: IInitializable
+{
+    private readonly SceneLoaderWrapper loader;
+
+    public EntryPoint(SceneLoaderWrapper loader)
+    {
+        this.loader = loader;
+    }
+
+    public void Initialize()
+    {
+        loader.LoadLevel(1);
+    }
+}
--- Assets/_____/Scripts/Installers/LobbySceneInstaller.cs
+++ Assets/_____/Scripts/Installers/LobbySceneInstaller.cs
@@ -1,31 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Zenject;
 
 public class LobbySceneInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
-        //Container.BindInterfacesAndSelfTo<LevelStateMachine>().AsSingle().NonLazy();
-        Container.BindInterfacesAndSelfTo<SimpleTouchInput>().AsSingle().NonLazy();
         Container.BindInterfacesAndSelfTo<Lobby>().AsSingle().NonLazy();
-
-    }
-}
-
-public class EntryPoint: IInitializable
-{
-    private readonly SceneLoaderWrapper loader;
-
-    public EntryPoint(SceneLoaderWrapper loader)
-    {
-        this.loader = loader;
-    }
-
-    public void Initialize()
-    {
-        Debug.Log("Load");
-        loader.LoadLevel(1);
     }
 }
